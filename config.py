0e3f00d6f11a4268092274d80e459cd9091f7f5f
denis313 <72709060+denis313@users.noreply.github.com>
2023-11-05 23:21:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher
 env = Env()
 env.read_env()
 
-bot_token = '5196838263:AAGqNO2Q-BpsQHgKhTf4DZDQt-e4TLQRKkU'
+# bot_token = '5196838263:AAGqNO2Q-BpsQHgKhTf4DZDQt-e4TLQRKkU'
 # ykassa = env('YKASSA')
 admins_ids = [1087502760]
 
