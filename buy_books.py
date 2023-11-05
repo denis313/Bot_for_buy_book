d2086ea6b2d08070ce893805bb4f329b22ec8447
Denis Evtushok <evtushokdenis2004@gmail.com>
2023-11-05 22:40:52 +0300

    Initial commit

--- buy_books.py
+++ buy_books.py
@@ -5,14 +5,13 @@ from handlers.clients import clients_list_books
 from handlers import start_bot_command
 
 
+# Основная функция
 async def main():
     dp.include_router(start_bot_command.router)
     dp.include_routers(admin_list_books.router,
                        admin_add_book.router,
                        admin_del_book.router)
     dp.include_router(clients_list_books.router)
-                        #clients_search.router,
-                        #clients_basket.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
